f016a4f9abc7f128aa865a7b243773e52e5294a5
Lan Le <lanle793@gmail.com>
2023-03-19 16:42:25 -0400

    add func to calculate points

--- upAndDownTheRiver.go
+++ upAndDownTheRiver.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome to the card game: Up and Down the River!!!")
+	fmt.Println(getPointsForRound(1, 1))
+}
+
+func getPointsForRound(guess int, wins int) int {
+	if wins == guess {
+		return 10 + wins
+	} else {
+		return 0
+	}
 }
